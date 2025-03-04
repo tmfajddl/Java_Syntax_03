6eb4948142914fbbea97d62ad875de06a9bcf980
tmfajddl <tmfajddl12@gmail.com>
2025-03-04 13:38:28 +0900

    조건문예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,25 @@
 package org.example;
 
-// 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요.
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        // `int a` => a라는 이름의 변수(즉 공간)을 만든다.
-        // `int` => a라는 공간에는 정수만 담을 수 있다.
-        // `;` => 문장이 끝남을 알린다.
-        int a;
-        a = 5;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("나이를 입력하세요 : ");
+        int num = sc.nextInt();
+        if(num<0)
+            System.out.println("나이를 다시입력해주세요.");
+        else if(num<10)
+            System.out.println("영유아입니다.");
+        else if(num<20)
+            System.out.println("10대입니다.");
+        else if(num<30)
+            System.out.println("20대입니다.");
+        else if(num<40)
+            System.out.println("30대입니다.");
+        else
+            System.out.println("40대이상입니다");
 
-        System.out.println(a);
-        System.out.println(a + 10);
-
-        int b = 10;
-
-        // `+` => 문장과 문자을 합친다.
-        System.out.println("a : " + a);
-        System.out.println("b : " + b);
-
-        // 여기서 부터
-        a = b;
-        b = 5;
-        // 여기까지 수정 가능
-
-        System.out.println("a : " + a);
-        // 출력 : a : 10
-        System.out.println("b : " + b);
-        // 출력 :  b : 5
-
-        // 문제 : a와 b가 가지고 있는 값을 서로 뒤바꿔주세요. // 조건 : 숫자와 사칙연산 사용 금지
-        a = 5;
-        b = 10;
-
-        // 여기서 부터
-        int c = a;
-        a = b;
-        b = c;
-        // 여기까지 수정 가능
-
-        System.out.println("a : " + a);
-        // 출력 : a : 10
-        System.out.println("b : " + b);
-        // 출력 :  b : 5
-    }
-}
+        sc.close();
+            }
+        }
