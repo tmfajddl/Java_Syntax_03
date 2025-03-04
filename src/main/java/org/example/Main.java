ac175e73857f7924a3f18fb59c143752d8b579ea
tmfajddl <tmfajddl12@gmail.com>
2025-03-04 14:57:04 +0900

    점수합격예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("월을 입력하세요 : ");
-        int mon = sc.nextInt();
-        if(mon<0)
-            System.out.println("월이 잘못 입력되었습니다.");
-        else if(mon <3)
-            System.out.println("겨울입니다.");
-        else if(mon<6)
-            System.out.println("봄입니다.");
-        else if(mon<9)
-            System.out.println("여름입니다.");
-        else if(mon<10)
-            System.out.println("가을입니다.");
-        else if(mon<=12)
-            System.out.println("겨울입니다.");
+        System.out.print("국어점수 : ");
+        int sco1 = sc.nextInt();
+        System.out.print("수학점수 : ");
+        int sco2 = sc.nextInt();
+        System.out.print("영어점수 : ");
+        int sco3 = sc.nextInt();
+        double avg = (sco1 + sco2 + sco3)/3;
+        if(sco1 >= 40 && sco2 >= 40 && sco3 >= 40) {
+            if (avg >= 60) {
+                System.out.println("국어 : " + sco1);
+                System.out.println("수학 : " + sco2);
+                System.out.println("영어 : " + sco3);
+                System.out.println("평균 : " + avg);
+                System.out.println("축하합니다, 합격입니다!");
+            }
+        }
         else
-            System.out.println("월이 잘못 입력되었습니다.");
+            System.out.println("불합격입니다.");
     }
 }
