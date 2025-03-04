38da214514a62d61ae843956ee1807446404ad71
tmfajddl <tmfajddl12@gmail.com>
2025-03-04 09:52:55 +0900

    25.03.04

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("V1");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("V2");
+        System.out.println("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.");
+        System.out.println("V3");
+        System.out.println("안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n");
+    }
+}
