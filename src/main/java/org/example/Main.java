a6aad15aecd8b27e83b7fd24e9cd529269fbdf56
tmfajddl <tmfajddl12@gmail.com>
2025-03-04 14:40:41 +0900

    계절출력예제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("나이를 입력하세요 : ");
-        int num = sc.nextInt();
-        if(num<0)
-            System.out.println("나이를 다시입력해주세요.");
-        else if(num<10)
-            System.out.println("영유아입니다.");
-        else if(num<20)
-            System.out.println("10대입니다.");
-        else if(num<30)
-            System.out.println("20대입니다.");
-        else if(num<40)
-            System.out.println("30대입니다.");
+        System.out.print("월을 입력하세요 : ");
+        int mon = sc.nextInt();
+        if(mon<0)
+            System.out.println("월이 잘못 입력되었습니다.");
+        else if(mon <3)
+            System.out.println("겨울입니다.");
+        else if(mon<6)
+            System.out.println("봄입니다.");
+        else if(mon<9)
+            System.out.println("여름입니다.");
+        else if(mon<10)
+            System.out.println("가을입니다.");
+        else if(mon<=12)
+            System.out.println("겨울입니다.");
         else
-            System.out.println("40대이상입니다");
-
-        sc.close();
-            }
-        }
+            System.out.println("월이 잘못 입력되었습니다.");
+    }
+}
