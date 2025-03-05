cc20a1df2f23055908ec867a27a4b853a1fbf2d2
tmfajddl <tmfajddl12@gmail.com>
2025-03-05 12:35:53 +0900

    1000부터1까지8단출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,15 @@
 package org.example;
 
-import java.util.Scanner;
-
 // 문제 : 아래와 같이 출력해주세요.
 // 문제 : 단을 * 1000까지 출력해주세요.
 
 class Main {
     public static void main(String[] args) {
-        int sum = 0;
-        int i = -100;
-        while(i<=25) {
-            sum += i;
-        i++;
+        int dan = 8;
+        int i = 1000;
+        while(i>0) {
+            System.out.println(dan + "*" + i + "=" + dan*i);
+        i--;
         }
-        System.out.println(sum);
     }
 }
