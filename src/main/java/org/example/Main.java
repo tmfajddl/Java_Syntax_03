d9fc2e00cbd89f55e28bf3ef51bfc437e42a9e75
tmfajddl <tmfajddl12@gmail.com>
2025-03-05 14:51:07 +0900

    직각삼각형 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,10 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("가로의 길이를 입력하시오 : ");
+        System.out.print("숫자를 입력하시오 : ");
         int n = sc.nextInt();
-        System.out.print("세로의 길이를 입력하시오 : ");
-        int m = sc.nextInt();
-        for(int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println("");
