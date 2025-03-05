9b929d5df610b592c1141b232baef80bc34523ce
tmfajddl <tmfajddl12@gmail.com>
2025-03-05 14:14:25 +0900

    빗금 간격이 3인 정사각형 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,129 @@
 package org.example;
 
-// 문제 : 아래와 같이 출력해주세요.
-// 문제 : 단을 * 1000까지 출력해주세요.
+import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        int dan = 8;
-        int i = 1;
-        while(i<=10) {
-            int j = 1;
-            while(j<=3){
-                System.out.println(j);
-                j++;
-            }
-        i++;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = 3;
+        for(int b = 1; b <= n; b++){
+            System.out.print("*");
         }
+        System.out.println("");
+        if((n-2) % 3 == 0) {
+            for (int a = 1; a <= n / k; a++) {
+                System.out.print("*");
+                for (int i = 2; i <= n - 1; i++) {
+                    if (i % k == 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int j = 2; j <= n - 1; j++) {
+                    if (j % k == 2)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int l = 2; l <= n - 1; l++) {
+                    if (l % k == 0)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+        else if((n-2) % 3 == 1) {
+            for (int a = 1; a <= n / k; a++) {
+                System.out.print("*");
+                for (int i = 2; i <= n - 1; i++) {
+                    if (i % k == 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int j = 2; j <= n - 1; j++) {
+                    if (j % k == 2)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int l = 2; l <= n - 1; l++) {
+                    if (l % k == 0)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+            System.out.print("*");
+            for (int i = 2; i <= n - 1; i++) {
+                if (i % k == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+        else{
+            for (int a = 1; a <= (n-2) / k; a++) {
+                System.out.print("*");
+                for (int i = 2; i <= n - 1; i++) {
+                    if (i % k == 1)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int j = 2; j <= n - 1; j++) {
+                    if (j % k == 2)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+                System.out.print("*");
+                for (int l = 2; l <= n - 1; l++) {
+                    if (l % k == 0)
+                        System.out.print("*");
+                    else
+                        System.out.print(" ");
+                }
+                System.out.println("*");
+
+            }
+            System.out.print("*");
+            for (int i = 2; i <= n - 1; i++) {
+                if (i % k == 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("*");
+            System.out.print("*");
+            for (int j = 2; j <= n - 1; j++) {
+                if (j % k == 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println("*");
+        }
+
+        for(int c = 1; c <= n; c++) {
+            System.out.print("*");
+        }
+
     }
 }
