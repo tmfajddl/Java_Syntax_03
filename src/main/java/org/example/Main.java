fc66fc1d44c0294b4318999651cdabcb9b5f13e6
tmfajddl <tmfajddl12@gmail.com>
2025-03-05 15:25:42 +0900

    피라미드 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,10 @@ class Main {
         System.out.print("숫자를 입력하시오 : ");
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= i; j++){
+            for(int k = 1; k <= (n-i); k++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*i-1; j++){
                 System.out.print("*");
             }
             System.out.println("");
