94acdd9d4a7d4cff21bab6777d73c182e37048ac
tmfajddl <tmfajddl12@gmail.com>
2025-03-05 12:32:52 +0900

    -100부터25까지합출력반복문

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         int sum = 0;
-        int i = 1;
-        while(i<=5) {
+        int i = -100;
+        while(i<=25) {
             sum += i;
         i++;
         }
