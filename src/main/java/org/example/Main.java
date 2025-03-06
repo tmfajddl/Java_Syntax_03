f33b6bfa9ca28b0562a47760f036253f04248cc2
tmfajddl <tmfajddl12@gmail.com>
2025-03-06 10:22:54 +0900

    계단만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("숫자를 입력하시오 : ");
+        System.out.println("숫자를 입력하세요 : ");
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++){
-            for(int k = 1; k <= (n-i); k++){
+        System.out.println("**");
+        for(int i = 1; i <= n-1; i++){
+            for(int j = 1; j <= i; j++)
                 System.out.print(" ");
-            }
-            for(int j = 1; j <= 2*i-1; j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+            System.out.println("**");
         }
     }
 }
+
