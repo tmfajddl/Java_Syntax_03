0c1b95158b78b9366df0929d5d5be7f22efcca41
tmfajddl <tmfajddl12@gmail.com>
2025-03-06 11:26:25 +0900

    지그재그만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,32 +7,19 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("높이를 입력하세요 : ");
-        int n = sc.nextInt();
-        System.out.print("밑변을 입력하세요 : ");
-        int m = sc.nextInt();
-        System.out.print("방향을 입력하세요 : ");
-        String h = sc.next();
-        if (Objects.equals(h, "R")) {
-            for (int i = n; i >= 1; i--) {
-                for (int k = 1; k <= i - 1; k++) {
+        int h = sc.nextInt();
+        System.out.print("반복횟수를 입력하세요 : ");
+        int r = sc.nextInt();
+        for(int i = 1; i <= r; i++){
+            for(int j = 1; j <= h; j++){
+                for(int n = 1; n < j; n++)
                     System.out.print(" ");
-                }
-                for (int j = 1; j <= m; j++)
-                    System.out.print("*");
-                System.out.println("");
+                System.out.println("*");
             }
-        }
-        else if (Objects.equals(h, "L")) {
-            for (int j = 1; j <= m; j++)
-                System.out.print("*");
-            System.out.println("");
-            for(int i = 1; i <= n-1; i++){
-                for (int k = 1; k <= i; k++) {
+            for(int m = h-1; m >= 1; m--){
+                for(int k = h-m; k <= 1; k++)
                     System.out.print(" ");
-                }
-                for (int j = 1; j <= m; j++)
-                    System.out.print("*");
-                System.out.println("");
+                System.out.println("*");
             }
         }
     }
