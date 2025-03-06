5e22bf56c799e170e4b6eb7907bc1bb15ec85691
tmfajddl <tmfajddl12@gmail.com>
2025-03-06 10:54:50 +0900

    평행사변형만들기2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,41 @@
 package org.example;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("숫자를 입력하세요 : ");
+        System.out.print("높이를 입력하세요 : ");
         int n = sc.nextInt();
-        for(int i = n; i >= 1; i--) {
-            for (int k = 1; k <= i - 1; k++) {
-                System.out.print(" ");
+        System.out.print("밑변을 입력하세요 : ");
+        int m = sc.nextInt();
+        System.out.print("방향을 입력하세요 : ");
+        String h = sc.next();
+        if (Objects.equals(h, "R")) {
+            for (int i = n; i >= 1; i--) {
+                for (int k = 1; k <= i - 1; k++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= m; j++)
+                    System.out.print("*");
+                System.out.println("");
             }
-            for (int j = 1; j <= n; j++)
+        }
+        else if (Objects.equals(h, "L")) {
+            for (int j = 1; j <= m; j++)
                 System.out.print("*");
             System.out.println("");
+            for(int i = 1; i <= n-1; i++){
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= m; j++)
+                    System.out.print("*");
+                System.out.println("");
+            }
         }
     }
 }
+
 
