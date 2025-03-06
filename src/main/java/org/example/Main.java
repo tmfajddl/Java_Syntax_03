20a06c79243d0d5cde08917ae36632c4fa0f0ecd
tmfajddl <tmfajddl12@gmail.com>
2025-03-06 13:04:59 +0900

    마름모 출력2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,19 @@ class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("정수를 입력하세요 : ");
         int n = sc.nextInt();
-        for(int i = n; i >= 1; i--){
-            for(int j = 1; j <= i-1; j++)
+        for(int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i - 1; j++)
                 System.out.print(" ");
-            System.out.print("*");
-            for(int k = 2*i; k <= 2*n-1; k++)
-                System.out.print(" ");
-            System.out.println("*");
+            for (int k = 2*i; k <= 2*n; k++)
+                System.out.print("*");
+            System.out.println("");
         }
-        for(int m = 1; m <= n; m++){
-            for(int j = 1; j < m; j++)
+        for(int m = 1; m <= n-1; m++){
+            for(int j = 1; j <= m; j++)
                 System.out.print(" ");
-            System.out.print("*");
-            for(int k = 2*m; k < 2*n; k++)
-                System.out.print(" ");
-            System.out.println("*");
+            for(int k = 2*m; k <= 2*n-2; k++)
+                System.out.print("*");
+            System.out.println("");
         }
     }
 }
