fe921dbbc3eaca1c4fd298f7a6fc5e2a96636447
tmfajddl <tmfajddl12@gmail.com>
2025-03-07 16:39:17 +0900

    배열생성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        int n = 10, firstTerm = 0, secondTerm = 1;
-
-        for (int i = 1; i <= n; ++i) {
-            System.out.println(firstTerm + ", ");
-
-            int nextTerm = firstTerm + secondTerm;
-            firstTerm = secondTerm;
-            secondTerm = nextTerm;
-        }
+        boolean[] arr1 = new boolean[]{true, false, true};
+        double[] arr2 = new double[]{3.14,7.77,11.11};
+        int[] arr3 = new int[10];
+        for(int i = 0; i < arr3.length; i++)
+            arr3[i] = i+1;
+        System.out.println("arr1");
+        for(int i = 0; i < arr1.length; i++)
+            System.out.print(arr1[i]+ " ");
+        System.out.println("");
+        System.out.println("arr2");
+        for(int i = 0; i < arr2.length; i++)
+            System.out.print(arr2[i] + " ");
+        System.out.println("");
+        System.out.println("arr3");
+        for(int i = 0; i < arr3.length; i++)
+            System.out.print(arr3[i]+ " ");
+        System.out.println("");
     }
 }
 
