721749d2f15a03c1ef54b4efaacc8c7887ec4277
tmfajddl <tmfajddl12@gmail.com>
2025-03-07 11:24:15 +0900

    제곱근 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,16 @@
 package org.example;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("가로 길이 입력: ");
-        int n = sc.nextInt();
-        System.out.print("세로 길이 입력: ");
-        int m = sc.nextInt();
-        System.out.print("+");
-        for(int i = 1; i <= n-2; i++)
-            System.out.print("-");
-        System.out.println("+");
-        for(int j = 1; j <= m-2; j++) {
-            System.out.print("|");
-            for (int k = 1; k <= n - 2; k++)
-                System.out.print(" ");
-            System.out.println("|");
+        int base = 3;
+        long result = 1;
+
+        for (int exponent = 4; exponent != 0; --exponent) {
+            result = result * base;
         }
-        System.out.print("+");
-        for(int i = 1; i <= n-2; i++)
-            System.out.print("-");
-        System.out.println("+");
+       System.out.println("Answer : " + result);
     }
 }
 
