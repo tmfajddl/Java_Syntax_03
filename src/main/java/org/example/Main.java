332a56cb707f797d2941bd318f67b158f6098c72
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 15:16:30 +0900

    각도 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,22 +4,11 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        Car car2 = new Car();
-        Car car3 = new Car();
-        car1.max_v = 180;
-        car2.max_v = 100;
-        car3.max_v = 230;
-        car1.달리다();
-        car2.달리다();
-        car3.달리다();
+        Scanner sc = new Scanner(System.in);
+        int angle1 = sc.nextInt();
+        int angle2 = sc.nextInt();
 
+        int sum_angle = angle1 + angle2;
+        System.out.println(sum_angle % 360);
     }
 }
-
-class Car{
-    int max_v;
-    void 달리다(){
-        System.out.println("자동차가 최고속력 " + max_v + "로 달립니다.");
-    }
-    }
