a3fc2880f0787978d34f846b192161d6cc5557eb
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 15:59:21 +0900

    수 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,15 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int angle1 = sc.nextInt();
-        int angle2 = sc.nextInt();
+        int number = sc.nextInt();
+        int answer = 0;
+        int n = 0;
 
-        int sum_angle = angle1 + angle2;
-        System.out.println(sum_angle % 360);
+        for (int i = 0; number >= 100 ; i++) {
+            answer += number % 100;
+            number /= 100;
+        }
+        answer += number;
+        System.out.println(answer);
     }
 }
