66d546d146a3ff2e1e2058957641c6e54fb8766c
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 12:51:35 +0900

    클래스 만들고 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,41 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        boolean[] arr1 = new boolean[]{true, false, true};
-        double[] arr2 = new double[]{3.14,7.77,11.11};
-        int[] arr3 = new int[10];
-        for(int i = 0; i < arr3.length; i++)
-            arr3[i] = i+1;
-        System.out.println("arr1");
-        for(int i = 0; i < arr1.length; i++)
-            System.out.print(arr1[i]+ " ");
-        System.out.println("");
-        System.out.println("arr2");
-        for(int i = 0; i < arr2.length; i++)
-            System.out.print(arr2[i] + " ");
-        System.out.println("");
-        System.out.println("arr3");
-        for(int i = 0; i < arr3.length; i++)
-            System.out.print(arr3[i]+ " ");
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        Person person1 = new Person();
+        System.out.print("학생번호: ");
+        person1.num = sc.nextInt();
+        System.out.print("학생이름: ");
+        person1.name = sc.next(); sc.nextLine();
+        System.out.print("학생나이: ");
+        person1.age = sc.nextInt();
+        System.out.print("학생키: ");
+        person1.height = sc.nextDouble();
+        Person person2 = new Person();
+        System.out.print("학생번호: ");
+        person2.num = sc.nextInt();
+        System.out.print("학생이름: ");
+        person2.name = sc.next(); sc.nextLine();
+        System.out.print("학생나이: ");
+        person2.age = sc.nextInt();
+        System.out.print("학생키: ");
+        person2.height = sc.nextDouble();
+        person1.person();
+        person2.person();
+
     }
 }
 
-
+class Person{
+        int num;
+        String name;
+        int age;
+        double height;
+        void person(){
+        System.out.println("학생정보 ");
+        System.out.println("학생번호: " + num);
+        System.out.println("학생이름: " + name);
+        System.out.println("학생나이: " + age);
+        System.out.println("학생키: " + height);
+    }
+}
