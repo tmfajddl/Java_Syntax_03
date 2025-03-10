738ff7038a02ba0666d8e8c83cc65ba87876ca42
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 13:51:30 +0900

    자동차 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,41 +4,22 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Person person1 = new Person();
-        System.out.print("학생번호: ");
-        person1.num = sc.nextInt();
-        System.out.print("학생이름: ");
-        person1.name = sc.next(); sc.nextLine();
-        System.out.print("학생나이: ");
-        person1.age = sc.nextInt();
-        System.out.print("학생키: ");
-        person1.height = sc.nextDouble();
-        Person person2 = new Person();
-        System.out.print("학생번호: ");
-        person2.num = sc.nextInt();
-        System.out.print("학생이름: ");
-        person2.name = sc.next(); sc.nextLine();
-        System.out.print("학생나이: ");
-        person2.age = sc.nextInt();
-        System.out.print("학생키: ");
-        person2.height = sc.nextDouble();
-        person1.person();
-        person2.person();
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        car1.max_v = 180;
+        car2.max_v = 100;
+        car3.max_v = 230;
+        car1.달리다();
+        car2.달리다();
+        car3.달리다();
 
     }
 }
 
-class Person{
-        int num;
-        String name;
-        int age;
-        double height;
-        void person(){
-        System.out.println("학생정보 ");
-        System.out.println("학생번호: " + num);
-        System.out.println("학생이름: " + name);
-        System.out.println("학생나이: " + age);
-        System.out.println("학생키: " + height);
+class Car{
+    int max_v;
+    void 달리다(){
+        System.out.println("자동차가 최고속력 " + max_v + "로 달립니다.");
     }
-}
+    }
