7b5f711ca9f741ac0ddec75502b7bcb7494ca9dc
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 16:29:05 +0900

    자기소개 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,25 +4,29 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            String code = sc.next();
-            String lastFourWords = code.substring(code.length()-4, code.length());
+        사람 a = new 사람();
+        a.name = "김철수";
+        a.age = 23;
+        a.IsMarried = false;
+        사람 b = new 사람();
+        b.name = "김영수";
+        b.age = 25;
+        b.IsMarried = true;
+        a.introduce();
+        b.introduce();
+    }
+}
 
-            if(lastFourWords.equals("_eye")){
-                System.out.println("Ophthalmologyc");
-            }
-            else if(lastFourWords.equals("head")){
-                System.out.println("Neurosurgery");
-            }
-            else if(lastFourWords.equals("infl")){
-                System.out.println("Orthopedics");
-            }
-            else if(lastFourWords.equals("skin")){
-                System.out.println("Dermatology");
-            }
-            else{
-                System.out.println("direct recommendation");
-            }
+class 사람{
+    int age;
+    String name;
+    boolean IsMarried;
+
+    void introduce(){
+        System.out.println("===자기소개===");
+        System.out.printf("이름: %s\n",this.name);
+        System.out.printf("나이: %d\n",this.age);
+        System.out.printf("결혼유무: %b\n",this.IsMarried);
 
     }
 }
