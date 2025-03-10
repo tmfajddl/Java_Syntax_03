d6fe9e81fe9c9662769ec23108a4778304e4c25f
tmfajddl <tmfajddl12@gmail.com>
2025-03-10 16:14:27 +0900

    병과 분류

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,25 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        int answer = 0;
-        int n = 0;
+            Scanner sc = new Scanner(System.in);
+            String code = sc.next();
+            String lastFourWords = code.substring(code.length()-4, code.length());
 
-        for (int i = 0; number >= 100 ; i++) {
-            answer += number % 100;
-            number /= 100;
-        }
-        answer += number;
-        System.out.println(answer);
+            if(lastFourWords.equals("_eye")){
+                System.out.println("Ophthalmologyc");
+            }
+            else if(lastFourWords.equals("head")){
+                System.out.println("Neurosurgery");
+            }
+            else if(lastFourWords.equals("infl")){
+                System.out.println("Orthopedics");
+            }
+            else if(lastFourWords.equals("skin")){
+                System.out.println("Dermatology");
+            }
+            else{
+                System.out.println("direct recommendation");
+            }
+
     }
 }
