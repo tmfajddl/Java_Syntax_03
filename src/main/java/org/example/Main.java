b0a86ec913f7529122fadd76495f77213f65c403
tmfajddl <tmfajddl12@gmail.com>
2025-03-11 13:15:55 +0900

    계산 결과 출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,29 +4,39 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("전체 학생수 입력 : ");
-        int n = sc.nextInt();
-        int[] score = new int[n];
-        int[] score2 = new int[n];
-        for(int i = 0; i < score.length; i++) {
-            System.out.printf("%d번째 학생 점수 입력 : ",i+1);
-            score[i] = sc.nextInt();
-        }
-        for(int i = 0; i < score.length; i++) {
-            int num = 0;
-            for(int j = 0; j < score.length; j++){
-                if(score[i] > score[j])
-                    num++;
-            }
-            score2[num] = score[i];
-        }
-        for(int i = 0; i < score.length; i++)
-            System.out.printf("%d ", score2[i]);
-        System.out.println("");
-        System.out.printf("가장 큰 점수차는 %d점 입니다.",score2[n-1]-score2[0]);
+        int 결과;
+
+        결과 = 계산기.합(10, 20);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 30
+
+        결과 = 계산기.합(30, 20);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 50
+
+        결과 = 계산기.합(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 100
+
+        결과 = 계산기.차(30, 70);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : -40
+
+        결과 = 계산기.곱(3, 7);
+        System.out.println("결과 : " + 결과);
+        // 출력 => 결과 : 21
     }
 }
+class 계산기{
+    static int 합(int a, int b){
+        return a + b;
+    }
 
-
+    static int 차(int a, int b){
+        return a - b;
+    }
+    static int 곱(int a, int b){
+        return a * b;
+    }
+}
 
