080a3f75c7e916f36e6c7a6db46be2b1a9413487
tmfajddl <tmfajddl12@gmail.com>
2025-03-11 10:34:41 +0900

    행복

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,29 +4,29 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        사람 a = new 사람();
-        a.name = "김철수";
-        a.age = 23;
-        a.IsMarried = false;
-        사람 b = new 사람();
-        b.name = "김영수";
-        b.age = 25;
-        b.IsMarried = true;
-        a.introduce();
-        b.introduce();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("전체 학생수 입력 : ");
+        int n = sc.nextInt();
+        int[] score = new int[n];
+        int[] score2 = new int[n];
+        for(int i = 0; i < score.length; i++) {
+            System.out.printf("%d번째 학생 점수 입력 : ",i+1);
+            score[i] = sc.nextInt();
+        }
+        for(int i = 0; i < score.length; i++) {
+            int num = 0;
+            for(int j = 0; j < score.length; j++){
+                if(score[i] > score[j])
+                    num++;
+            }
+            score2[num] = score[i];
+        }
+        for(int i = 0; i < score.length; i++)
+            System.out.printf("%d ", score2[i]);
+        System.out.println("");
+        System.out.printf("가장 큰 점수차는 %d점 입니다.",score2[n-1]-score2[0]);
     }
 }
 
-class 사람{
-    int age;
-    String name;
-    boolean IsMarried;
 
-    void introduce(){
-        System.out.println("===자기소개===");
-        System.out.printf("이름: %s\n",this.name);
-        System.out.printf("나이: %d\n",this.age);
-        System.out.printf("결혼유무: %b\n",this.IsMarried);
 
-    }
-}
