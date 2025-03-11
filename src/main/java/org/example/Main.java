9d1c7d15b9bcd6965a27431387cbb510bb17c640
tmfajddl <tmfajddl12@gmail.com>
2025-03-11 15:06:37 +0900

    나이출력

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,24 @@
 package org.example;
 
-import java.awt.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Solution a = new Solution();
+        선생님 a = new 선생님();
         System.out.print("기준 년도 입력 : ");
         int year = sc.nextInt();
         System.out.print("나이 입력 : ");
         int age = sc.nextInt();
-        System.out.printf("%d년 기준 %d살이므로 %d년생입니다.",year, age, a.solution(age));
+        System.out.printf("%d년 기준 %d살이므로 %d년생입니다.",year, age, a.출생년도(year,age));
 
 
     }
 }
 
-class Solution {
-    public int solution(int age) {
-        int answer = 2022 - (age-1);
+class 선생님 {
+    public int 출생년도(int year, int age) {
+        int answer = year - (age-1);
         return answer;
     }
 }
