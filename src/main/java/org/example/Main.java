de75badc6545470a2ce6e06735e6316eb0234622
tmfajddl <tmfajddl12@gmail.com>
2025-03-12 16:10:51 +0900

    공격과 재공격

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,50 +1,34 @@
 package org.example;
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
+        전사 a전사 = new 전사();
 
-        오리 a오리 = new 오리();
-        a오리.날다();
+        a전사.공격("브라이언","칼");
+        // 브라이언이(가) 칼(으)로 공격합니다.
+        a전사.재공격();
 
-        청둥오리 a청둥오리 = new 청둥오리();
-        a청둥오리.날다();
-        // 출력 : 오리가 날개로 날아갑니다.
+        a전사.공격("필립","창");
+        // 필립이(가) 창(으)로 공격합니다.
 
-        흰오리 a흰오리 = new 흰오리();
-        a흰오리.날다();
-        // 출력 : 오리가 날개로 날아갑니다.
-
-        고무오리 a고무오리 = new 고무오리();
-        a고무오리.날다();
-        // 출력 : 저는 날 수 없어요. ㅜㅠ
-
-        고무2오리 a고무2오리 = new 고무2오리();
-        a고무2오리.날다();
-        // 출력 : 저는 날 수 없어요. ㅜㅠ
+        a전사.공격("마크","지팡이");
+        // 마크(가) 지팡이(으)로 공격합니다.
+        a전사.재공격();
+        // 마크(가) 지팡이(으)로 공격합니다.
     }
 }
 
-class 오리 {
-    void 날다(){
-        System.out.println("오리가 날개로 날아갑니다.");
+class 전사{
+    String name;
+    String tool;
+    void 공격(String name ,String tool){
+        this.name = name;
+        this.tool = tool;
+        System.out.printf("%s이(가) %s(으)로 공격합니다.\n",name,tool);
+    };
+    void 재공격() {
+        this.공격(this.name, this.tool);
     }
 }
-class 청둥오리 extends 오리{
-
-}
-class 흰오리 extends 오리{
-
-}
-class 고무오리 extends 오리{
-    void 날다(){
-        System.out.println("저는 날 수 없어요. ㅜㅠ");
-    }
-}
-class 고무2오리 extends 고무오리{
-
-}
-
 
 
