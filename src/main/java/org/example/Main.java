d791c262c0036b903fc80449a17027a9357e1d13
tmfajddl <tmfajddl12@gmail.com>
2025-03-13 11:21:19 +0900

    양꼬치

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,44 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 로봇.get정수();
-        boolean b = 로봇.get논리();
-        사람 a사람 = 로봇.get사람();
-        사람 a사람2 = 로봇.get사람2();
-        사람 a사람3 = 로봇.get사람3();
-        사람 a사람4 = 로봇.get사람4();
-        사람 a사람5 = 로봇.get사람5(123,false);
-        로봇.get사람6(123,false);
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("양꼬치 인분 수: ");
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        if(n>=10)
+            k -= n/10;
+        int sum = 12000 * n + 2000 * k;
+        System.out.printf("음식의 총 합은 %d원입니다.",sum);
     }
 }
-
-class 로봇 extends 사람{
-    static int get정수(){
-        return 0;
-    }
-    static boolean get논리(){
-        return true;
-    }
-    static 사람 get사람(){
-        return null;
-    }
-    static 사람 get사람2(){
-        return null;
-    }
-    static 사람 get사람3(){
-        return null;
-    }
-    static 사람 get사람4(){
-        return null;
-    }
-    static 사람 get사람5(int a, boolean c){
-        return null;
-    }
-    static 사람 get사람6(int a, boolean c){
-        return null;
-    }
-}
-class 사람{
-}
-
