f4101c87b37b9c2824c09d8ba557b96e6a544a52
tmfajddl <tmfajddl12@gmail.com>
2025-03-13 09:29:47 +0900

    리모컨

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,33 +2,105 @@ package org.example;
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        // 표준Tv 리모콘을 만든다.
+        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
 
-        a전사.공격("브라이언","칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
-        a전사.재공격();
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.공격("필립","창");
-        // 필립이(가) 창(으)로 공격합니다.
+        a샤오미Tv.켜기();
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기();
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기();
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.공격("마크","지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        a삼성Tv.켜기();
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기();
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기();
+        // 출력 => 삼성Tv ar켜기!
+
+        aLGTv.켜기();
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기();
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환();
+        // 출력 => LGTv 게임모드전환!
+
+        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
+
+        표준Tv a표준Tv;
+
+        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
+        a표준Tv = a샤오미Tv;
+        a표준Tv.켜기();
+        // 출력 : 샤오기TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 샤오기TV 꺼집니다.
+
+        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
+        a표준Tv = a삼성Tv;
+        a표준Tv.켜기();
+        // 출력 : 삼성TV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : 삼성TV 꺼집니다.
+
+        // a표준Tv 변수에 LGTv 객체를 연결한다.
+        a표준Tv = aLGTv;
+        a표준Tv.켜기();
+        // 출력 : LGTV 켜집니다.
+        a표준Tv.끄기();
+        // 출력 : LGTV 꺼집니다.
+
+        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
+        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
+        LGTv aLGTv2 = (LGTv)a표준Tv;
+        aLGTv2.게임모드전환();
     }
 }
 
-class 전사{
-    String name;
-    String tool;
-    void 공격(String name ,String tool){
-        this.name = name;
-        this.tool = tool;
-        System.out.printf("%s이(가) %s(으)로 공격합니다.\n",name,tool);
+class 표준Tv{
+    void 켜기(){
+    }
+    void 끄기(){
+    }
     };
-    void 재공격() {
-        this.공격(this.name, this.tool);
+class 삼성Tv extends 표준Tv{
+    void 켜기(){
+        System.out.println("삼성TV 켜집니다.");
+    }
+    void 끄기(){
+        System.out.println("삼성TV 꺼집니다.");
+    }
+    void ar켜기(){
+        System.out.println("삼성TV ar켜기!");
     }
 }
+class 샤오미Tv extends 표준Tv{
+    void 켜기(){
+        System.out.println("샤오미TV 켜집니다.");
+    }
+    void 끄기(){
+        System.out.println("샤오미TV 꺼집니다.");
+    }
+    void vr켜기(){
+        System.out.println("샤오미TV vr켜기!");
+    }
 
+}
+class LGTv extends 표준Tv{
+    void 켜기(){
+        System.out.println("LGTV 켜집니다.");
+    }
+    void 끄기(){
+        System.out.println("LGTV 꺼집니다.");
+    }
+    void 게임모드전환(){
+        System.out.println("LGTV 게임모드전환!");
+    }
+
+}
 
