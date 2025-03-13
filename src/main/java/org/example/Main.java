e70f2ed7f335af82b78ab897e75c942140aab543
tmfajddl <tmfajddl12@gmail.com>
2025-03-13 10:55:38 +0900

    리턴타입만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,106 +1,47 @@
 package org.example;
 
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        // 표준Tv 리모콘을 만든다.
-        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
+        int i = 로봇.get정수();
+        boolean b = 로봇.get논리();
+        사람 a사람 = 로봇.get사람();
+        사람 a사람2 = 로봇.get사람2();
+        사람 a사람3 = 로봇.get사람3();
+        사람 a사람4 = 로봇.get사람4();
+        사람 a사람5 = 로봇.get사람5(123,false);
+        로봇.get사람6(123,false);
 
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
-
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
-
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
-
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-
-        표준Tv a표준Tv;
-
-        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오기TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오기TV 꺼집니다.
-
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-        LGTv aLGTv2 = (LGTv)a표준Tv;
-        aLGTv2.게임모드전환();
     }
 }
 
-class 표준Tv{
-    void 켜기(){
+class 로봇 extends 사람{
+    static int get정수(){
+        return 0;
     }
-    void 끄기(){
+    static boolean get논리(){
+        return true;
     }
-    };
-class 삼성Tv extends 표준Tv{
-    void 켜기(){
-        System.out.println("삼성TV 켜집니다.");
+    static 사람 get사람(){
+        return null;
     }
-    void 끄기(){
-        System.out.println("삼성TV 꺼집니다.");
+    static 사람 get사람2(){
+        return null;
     }
-    void ar켜기(){
-        System.out.println("삼성TV ar켜기!");
+    static 사람 get사람3(){
+        return null;
+    }
+    static 사람 get사람4(){
+        return null;
+    }
+    static 사람 get사람5(int a, boolean c){
+        return null;
+    }
+    static 사람 get사람6(int a, boolean c){
+        return null;
     }
 }
-class 샤오미Tv extends 표준Tv{
-    void 켜기(){
-        System.out.println("샤오미TV 켜집니다.");
-    }
-    void 끄기(){
-        System.out.println("샤오미TV 꺼집니다.");
-    }
-    void vr켜기(){
-        System.out.println("샤오미TV vr켜기!");
-    }
-
-}
-class LGTv extends 표준Tv{
-    void 켜기(){
-        System.out.println("LGTV 켜집니다.");
-    }
-    void 끄기(){
-        System.out.println("LGTV 꺼집니다.");
-    }
-    void 게임모드전환(){
-        System.out.println("LGTV 게임모드전환!");
-    }
-
+class 사람{
 }
 
