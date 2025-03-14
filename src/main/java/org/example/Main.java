8f3347d0f75030bc6bf41493336d9b8e86baa934
tmfajddl <tmfajddl12@gmail.com>
2025-03-14 09:12:55 +0900

    배열 입력받아 값이 두배인 배열 생성

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,63 +1,21 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
-
-        String 이름 = "칸";
-        a전사.이름 = 이름;
-        a전사.나이 = 20;
-        a전사.자기소개();
-
-        a전사.나이++;
-        a전사.자기소개();
-
-        a전사.나이 = 30;
-        a전사.이름 = "카니";
-        a전사.자기소개();
-
-        a전사.a무기 = new 활();
-        a전사.공격();
-        // 출력 : 카니가 활로 공격합니다.
-
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        // 출력 : 카니가 칼로 공격합니다.
-    }
-}
-
-class 전사 {
-    // 인스턴스 변수
-    String 이름;
-    // 인스턴스 변수
-    int 나이;
-    // 인스턴스 변수
-    무기 a무기;
-
-    void 자기소개() {
-        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
-    }
-    void 공격(){
-        a무기.이름2 = 이름;
-        this.a무기.공격2();
-    }
-}
-
-class 무기 {
-    String 이름2;
-    void 공격2(){}
-}
-
-class 칼 extends 무기 {
-    void 공격2(){
-        System.out.printf("%s가 칼로 공격합니다.\n",이름2);
-    }
-}
-
-class 활 extends 무기 {
-    void 공격2(){
-        System.out.printf("%s가 활로 공격합니다.\n",이름2);
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[5];
+        int len = numbers.length;
+        for(int i = 0; i < len; i++) {
+            System.out.print("배열에 들어갈 숫자를 입력하세요 : ");
+            numbers[i] = sc.nextInt();
+        }
+        int[] arr = new int[len];
+        for(int i = 0; i < len; i++)
+            arr[i] = numbers[i] * 2;
+        for(int i = 0; i < len; i++)
+            System.out.printf("%d ",arr[i]);
     }
 }
