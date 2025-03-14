bfdb9e2bbe94b900ef13655c55869a9199e8ae4a
tmfajddl <tmfajddl12@gmail.com>
2025-03-14 09:27:37 +0900

    배열 입력받아 배열 뒤집기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,19 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numbers = new int[5];
-        int len = numbers.length;
+        int[] num_list = new int[5];
+        int len = num_list.length;
         for(int i = 0; i < len; i++) {
             System.out.print("배열에 들어갈 숫자를 입력하세요 : ");
-            numbers[i] = sc.nextInt();
+            num_list[i] = sc.nextInt();
         }
         int[] arr = new int[len];
-        for(int i = 0; i < len; i++)
-            arr[i] = numbers[i] * 2;
+        for(int i = len - 1; i >= 0; i--){
+            for(int j = 0; j < len; j++){
+                if(i + j == len - 1)
+                    arr[j] = num_list[i];
+            }
+        }
         for(int i = 0; i < len; i++)
             System.out.printf("%d ",arr[i]);
     }
