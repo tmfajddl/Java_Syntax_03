c177b88f486893046d5c7efff1bd098443eba3dd
tmfajddl <tmfajddl12@gmail.com>
2025-03-17 16:05:42 +0900

    A+B 무한루프

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,14 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum_price = sc.nextInt();
-        int n = sc.nextInt();
-        int sum = 0;
-        for(int i = 1; i <= n; i++) {
-            int price = sc.nextInt();
-            int num = sc.nextInt();
-            sum += price * num;
+        int i = 1;
+        while (true){
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            if(num1 == 0 && num2 == 0)
+                break;
+            System.out.println(num1 + num2);
+            i++;
         }
-        if(sum == sum_price)
-            System.out.println("YES");
-        else
-            System.out.println("NO");
     }
 }
